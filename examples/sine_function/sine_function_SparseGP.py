27c97af96aa69aa90d6494313e1b8d048483ae10
deri0s <derios8@outlook.com>
2024-05-08 18:03:11 +0100

    Sparse GP on the Synthetic dataset workig

--- examples/sine_function/sine_function_SparseGP.py
+++ examples/sine_function/sine_function_SparseGP.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from pathlib import Path
 import pandas as pd
 
@@ -47,7 +49,7 @@ c2 = [int(i) for i in c2]
 indices = [c0, c1, c2]
 
 # standardise data
-X=np.vstack(X)
+X = np.vstack(X)
 y_mu = np.mean(Y)
 y_std = np.std(Y)
 Y = np.vstack((Y - y_mu) / y_std)
@@ -63,49 +65,83 @@ likelihood = gpx.likelihoods.Gaussian(num_datapoints=D.n)
 
 posterior = prior * likelihood
 
-negative_mll = gpx.objectives.ConjugateMLL(negative=True)
-negative_mll(posterior, train_data=D)
+"""
+    SPARSE APPROXIMATION
+"""
+import optax as ox
+import jax.random as jr
+from sklearn.metrics import mean_squared_error
 
-negative_mll = jit(negative_mll)
+# If n_inducing points is close to N, the model will not return
+# accurate solutions.
+n_inducing = 20
+z = np.linspace(X.min(), X.max(), n_inducing).reshape(-1, 1)
 
-# Optimiser
-opt_posterior, history = gpx.fit_scipy(model=posterior,
-                                       objective=negative_mll,
-                                       train_data=D)
+q = gpx.variational_families.CollapsedVariationalGaussian(posterior=posterior,
+                                                          inducing_inputs=z)
+
+elbo = gpx.objectives.CollapsedELBO(negative=True)
+elbo = jit(elbo)
+
+# Training
+opt_posterior, history = gpx.fit(
+    model=q,
+    objective=elbo,
+    train_data=D,
+    optim=ox.adamw(learning_rate=1e-2),
+    num_iters=500,
+    key=jr.key(123),
+)
+
+# Model convergence
+fig, ax = plt.subplots()
+ax.plot(history, color='red')
+ax.set(xlabel="Training iterate", ylabel="ELBO")
 
 # Predictions
-latent_dist = opt_posterior.predict(xNew, train_data=D)
-predictive_dist = opt_posterior.likelihood(latent_dist)
+latent_dist = opt_posterior(xNew, train_data=D)
+predictive_dist = opt_posterior.posterior.likelihood(latent_dist)
+inducing_points = opt_posterior.inducing_inputs
 
 mu = predictive_dist.mean()
 std = predictive_dist.stddev()
 
 # CALCULATING THE OVERALL MSE
-from sklearn.metrics import mean_squared_error
-
 F = 150 * xNew * np.sin(xNew)
 F = np.vstack((F - np.mean(F)) / np.std(F))
-print("Mean Squared Error (GP)    : ", mean_squared_error(mu, F))
+print("Mean Squared Error (Sparse GP)    : ", mean_squared_error(mu, F))
 
 #-----------------------------------------------------------------------------
 # REGRESSION PLOT
 #-----------------------------------------------------------------------------
-plt.figure()
+fig, ax = plt.subplots()
+
+ax.vlines(
+    x=inducing_points,
+    ymin=Y.min(),
+    ymax=Y.max(),
+    alpha=0.3,
+    linewidth=1.5,
+    label="Inducing point",
+    color='orange',
+)
+
+plt.plot(xNew, F, color='black', linewidth = 4,
+         label='Sine function')
 
 plt.plot(X, Y, "x", label="Observations", color='black', alpha=0.5)
+
 plt.fill_between(
     xNew.squeeze(),
     mu - 2 * std,
     mu + 2 * std,
     alpha=0.2,
     label="Two sigma",
-    color='lightcoral')
-
-plt.plot(xNew, F, color='black', linewidth = 4,
-         label='Sine function')
+    color='lightcoral',
+)
 
 plt.plot(xNew, mu, color='red', linestyle='-', linewidth = 4,
-         label='DDPGP')
+         label='Sparse GP')
 plt.title('Regression Performance', fontsize=20)
 plt.xlabel('x', fontsize=16)
 plt.ylabel('f(x)', fontsize=16)
