d8df4eefd9b4f200e3fc9f0c424df35e6851af86
deri0s <derios8@outlook.com>
2025-01-18 18:31:19 +0000

    Discrepancy between y0 and y-raw in estimate file

--- src/case_study/manufacturing/estimate_test_data.py
+++ src/case_study/manufacturing/estimate_test_data.py
@@ -4,13 +4,12 @@ import time
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler as ss
 from case_study.manufacturing.data_and_preprocessing.raw import data_processing_methods as dpm
-from sklearn.decomposition import PCA
 
 """
 NSG data
 """
 # NSG post processes data location
-# file = 'data_and_preprocessing/processed/NSG_processed_data.xlsx'
+file_val = 'validation_data.xlsx'
 file = 'data_and_preprocessing/processed/NSG_processed_data_14_inputs.xlsx'
 
 # Training df
@@ -19,8 +18,26 @@ y_df = pd.read_excel(file, sheet_name='y')
 y_raw_df = pd.read_excel(file, sheet_name='y_raw')
 t_df = pd.read_excel(file, sheet_name='timelags')
 
+# validation df
+val_df = pd.read_excel(file_val)
+date_timev = val_df.date_time.values
+val_mu = val_df.gp_pred.values
+
+# get data on the region of interest (valitation data)
+first_date = val_df.date_time[0]
+last_date = val_df.date_time[len(val_df.date_time)-1]
+
+first = y_df[y_df['Time stamp'] == first_date].index[0]
+last  = y_df[y_df['Time stamp'] == last_date].index[0]
+
+X_df = X_df.iloc[first:last, :]
+y_df = y_df.iloc[first:last, :]
+
 # Pre-Process training data
 X, y0, N0, D, max_lag, time_lags = dpm.align_arrays(X_df, y_df, t_df)
+
+print('max-lag: ', max_lag)
+print('y0: ', len(y0), ' 20%: ', int(len(y0)*0.25), ' 3 days: 216')
 
 # Replace zero values with interpolation
 zeros = y_raw_df.loc[y_raw_df['raw_furnace_faults'] <= 1e-1]
@@ -32,42 +49,26 @@ y_raw = dpm.adjust_time_lag(y_raw_df['raw_furnace_faults'].values,
                             shift=0,
                             to_remove=max_lag)
 
-date_time0 = y_df['Time stamp'].values
-
 date_time = dpm.adjust_time_lag(y_df['Time stamp'].values,
                                 shift=0,
                                 to_remove=max_lag)
 
 # Train and test data
 N, D = np.shape(X)
-# '2020-08-14
-start_train = y_df[y_df['Time stamp'] == '2020-07-25-10'].index[0]
-end_train = y_df[y_df['Time stamp'] == '2020-08-27-14'].index[0]
+end_train = N - int(len(y0)*0.25)
 
-X_train, y_train = X[start_train:end_train], y_raw[start_train:end_train]
+X_train, y_train = X[0:end_train], y_raw[0:end_train]
+date_train = date_time[0:end_train]
 N_train = len(X_train)
 
-caca = y_df[y_df['Time stamp'] == '2020-08-29'].index[0] - start_train
-print('start-train: ', start_train, 'end-train: ', end_train,
-      ' caca: ', caca, ' caca + start-train: ', start_train + caca,
-      ' length: ', len(date_time))
+X_test = X[0:N]
+date_time = date_time[0:N]
+y_raw = y_raw[0:N]
+y_rect = y0[0:N]
 
-end_test1 = start_train + caca
-date_time1 = date_time[start_train:start_train + caca]
-X_test = X[start_train:end_test1]
-date_time = date_time[start_train:end_test1]
-y_raw = y_raw[start_train:end_test1]
-y_rect = y0[start_train:end_test1]
-
-# end_test = end_train + 200
-# X_test = X[start_train:end_test]
-# date_time = date_time[start_train:end_test]
-# y_raw = y_raw[start_train:end_test]
-# y_rect = y0[start_train:end_test]
-
-# #-----------------------------------------------------------------------------
-# # REGRESSION PLOT
-# #-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+# REGRESSION PLOT
+#-----------------------------------------------------------------------------
 
 fig, ax = plt.subplots()
 
@@ -77,15 +78,14 @@ plt.rc('xtick', labelsize=14)
 plt.rc('ytick', labelsize=14)
 fig.autofmt_xdate()
 
-ax.plot(date_time, y_raw, color='grey', label='Raw')
-ax.plot(date_time, y_rect, color='blue', label='Filtered')
-plt.axvline(date_time[N_train-1], linestyle='--', linewidth=3,
-            color='black')
-# plt.axvline(date_time[N_train+150-1], linestyle='--', linewidth=3,
-#             color='black')
-# plt.axvline(date_time[start_train + caca], linestyle='--', linewidth=3,
-#             color='orange', label='end-date')
+print(type(date_timev[0]))
+print(type(date_time[0]))
 
+ax.plot(date_timev, val_mu, color='green', label='Val')
+ax.plot(date_train, y_train, color='grey', label='Raw')
+ax.plot(date_time, y_rect, color='blue', label='Filtered')
+plt.axvline(date_time[end_train-1], linestyle='--', linewidth=3,
+            color='black')
 ax.set_xlabel(" Date-time", fontsize=14)
 ax.set_ylabel(" Fault density", fontsize=14)
 plt.legend(loc=0, prop={"size":18}, facecolor="white", framealpha=1.0)
---
+++ src/case_study/manufacturing/generate_validation_data.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler as ss
+from case_study.manufacturing.data_and_preprocessing.raw import data_processing_methods as dpm
+
+"""
+NSG data
+
+Choose:
+- NSG_processed_data.xlsx:           10 inpus
+- NSG_processed_data_14_inputs.xlsx: 14 inputs
+
+Do not adjust data for timelags.
+"""
+
+# NSG post processes data location
+# file = 'data_and_preprocessing/processed/NSG_processed_data.xlsx'
+file = 'data_and_preprocessing/processed/NSG_processed_data_14_inputs.xlsx'
+
+# Training df
+X_df = pd.read_excel(file, sheet_name='X_stand')
+y_df = pd.read_excel(file, sheet_name='y')
+y_raw_df = pd.read_excel(file, sheet_name='y_raw')
+
+# Pre-Process training data
+X    = X_df.values
+y0   = y_df.furnace_faults.values
+N, D = np.shape(X)
+
+# Replace zero values with interpolation
+zeros = y_raw_df.loc[y_raw_df['raw_furnace_faults'] <= 1e-1]
+y_raw_df.loc[zeros.index, 'raw_furnace_faults'] = None
+y_raw_df.interpolate(inplace=True)
+
+y_raw = y_raw_df['raw_furnace_faults'].values
+date_time = y_df['Time stamp'].values
+
+# Train and test data
+# Clean area:
+# Start: '2020-07-25-01', Ends: '2020-09-01-05'
+start_train = y_df[y_df['Time stamp'] == '2020-07-25-01'].index[0]
+end_train = y_df[y_df['Time stamp'] == '2020-09-01-05'].index[0]
+
+X_train, y_train = X[start_train:end_train], y_raw[start_train:end_train]
+N_train = len(X_train)
+
+X_test = X_train
+date_time = date_time[start_train:end_train]
+y_raw = y_raw[start_train:end_train]
+y_rect = y0[start_train:end_train]
+
+print(len(y_raw))
+
+#-----------------------------------------------------------------------------
+# PLOT TRAINING DATA
+#-----------------------------------------------------------------------------
+
+fig, ax = plt.subplots()
+
+# Increase the size of the axis numbers
+plt.rcdefaults()
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+fig.autofmt_xdate()
+
+ax.plot(date_time, y_raw, color='grey', label='Raw')
+ax.plot(date_time, y_rect, color='blue', label='Filtered')
+ax.set_xlabel(" Date-time", fontsize=14)
+ax.set_ylabel(" Fault density", fontsize=14)
+plt.legend(loc=0, prop={"size":18}, facecolor="white", framealpha=1.0)
+
+"""
+DPSGP cleaning
+
+GPytotch is very sensitive to the initial hyperparameters.
+I first used the DPGP sklearn version to estimate the initial
+lengthscales for this script.
+"""
+import torch
+from gpytorch.likelihoods import GaussianLikelihood
+from gpytorch.means import ConstantMean
+from gpytorch.kernels import SpectralMixtureKernel as SM
+from gpytorch.kernels import InducingPointKernel, ScaleKernel, RBFKernel as RBF
+from models.dpsgp_gpytorch import DirichletProcessSparseGaussianProcess as DPSGP
+
+# Convert data to torch tensors to input inducing points
+floating_point = torch.float64
+X_tensor = torch.tensor(X_train, dtype=floating_point)
+inducing_points = X_tensor[::10, :]
+
+likelihood = GaussianLikelihood()
+
+se = ScaleKernel(RBF(ard_num_dims=X_train.shape[-1]))
+
+covar_module = InducingPointKernel(se,
+                                   inducing_points=inducing_points,
+                                   likelihood=likelihood)
+
+# 10 inputs
+# lss = [0.284, 1.54e+04, 0.48, 0.662, 2.79e+04, 337, 4.86e+04, 3.71e+04, 1.13, 0.25]
+# lss = [1e+05, 342, 516, 0.468, 0.25, 6.57e+04, 1.33e+03, 0.878, 1.07, 4.71e+03]
+# lss = [2.6, 0.963, 1e+05, 0.679, 1e+05, 5.25, 0.25, 4.05e+04, 2, 575]
+
+# 14 inputs
+# lss = [1.83, 0.318, 603, 0.651, 5.87e+04, 3.0, 1.17, 1.2e+03, 4.63, 0.25, 1.19e+04, 52.2, 663, 17.3]
+# lss = [2.83, 1.318, 603, 1.651, 5.87e+04, 3.0, 10.17, 1.2e+03, 4.63, 25, 1.19e+04, 52.2, 663, 17.3]
+lss = [1.83, 0.8, 603, 1.1, 5.87e+04, 3.0, 2.17, 1.2e+03, 4.63, 1, 1.19e+04, 52.2, 663, 17.3]
+start_time = time.time()
+sgp = DPSGP(X_train, y_train, init_K=7,
+            gp_model='Sparse',
+            prior_mean=ConstantMean(), kernel=covar_module,
+            lengthscale=lss, #0.05*np.ones(X.shape[-1]),
+            N_iter=15,
+            noise_var = 0.06,
+            floating_point=floating_point,
+            normalise_y=True,
+            DP_max_iter=390,
+            print_conv=True, plot_conv=True, plot_sol=True)
+sgp.train()
+mu, stds = sgp.predict(X_test)
+comp_time = time.time() - start_time
+
+print(f'DPSGP cleaning time: {comp_time:.2f} seconds')
+
+print('\n Furnace parameters relevance')
+d = {'Features': X_df.columns, 'Importance': sgp.lengthscale[0]}
+fidf = pd.DataFrame.from_dict(d)
+fidf = fidf.sort_values(by='Importance')
+print(fidf.head(14))
+
+# get inducing points indices
+_z_indices = sgp._z_indices
+
+print('N-induced: ', len(_z_indices))
+
+# # save predictions to use it in another scipt as the `true` fault_density
+d = {"date_time": date_time, "gp_pred": mu}
+
+df = pd.DataFrame(d)
+df.to_excel("validation_data.xlsx")
+
+#-----------------------------------------------------------------------------
+# REGRESSION PLOT
+#-----------------------------------------------------------------------------
+fig, ax = plt.subplots()
+
+# Increase the size of the axis numbers
+plt.rcdefaults()
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+fig.autofmt_xdate()
+
+ax.fill_between(date_time,
+                mu + 3*stds, mu - 3*stds,
+                alpha=0.5, color='lightcoral',
+                label='3$\\sigma$')
+ax.plot(date_time, y_raw, color='grey', label='Raw')
+ax.plot(date_time, y_rect, color='blue', label='Filtered')
+ax.plot(date_time, mu, color="red", linewidth = 2.5, label="Cleaned")
+
+ax.vlines(
+    x=date_time[::10],
+    ymin=-0.5,
+    ymax=y_train.max().item(),
+    alpha=0.3,
+    linewidth=1.5,
+    ls='--',
+    label="z0",
+    color='grey'
+)
+dt0 = date_time[sgp.indices[0]]
+print('N-clean: ', len(dt0))
+
+ax.vlines(
+    # Sparse clean data
+    x=dt0[_z_indices],
+    ymin=-0.5,
+    ymax=y_train.max().item(),
+    alpha=0.3,
+    linewidth=1.5,
+    label="z*",
+    color='orange'
+)
+ax.set_xlabel(" Date-time", fontsize=14)
+ax.set_ylabel(" Fault density", fontsize=14)
+plt.legend(loc=0, prop={"size":18}, facecolor="white", framealpha=1.0)
+plt.show()
